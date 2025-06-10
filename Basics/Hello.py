1d0669e91fceb3da8cfeea75c905011c92ace7a0
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-06-10 08:40:53 +0530

    Starting Python

---
+++ Basics/Hello.py
@@ -0,0 +1,10 @@
+## data types and variables
+print("Hello world")
+a = 2
+b= 3
+c= 4
+d = None
+print(d)
+e = input("Enter a number : ")
+f = input("Enter another number : ")
+print( int(e)*int(f) , " is the product of the input")
